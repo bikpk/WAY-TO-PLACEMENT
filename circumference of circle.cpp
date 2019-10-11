dcf72b742a6def76601604c7d5d6ecf8faef03d9
Pradeep kumar <56067863+bikpk@users.noreply.github.com>
2019-10-11 21:53:55 +0530

    Add files via upload

---
+++ circumference of circle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int radius;
+	float pi=3.14;
+	printf("enter the radius of circle\n");
+	scanf("%d",&radius);
+	float ci;
+	ci=2*pi*radius;
+	printf("the cirumfernce of circle is:%f",ci);
+	return 0;
+	
+}
